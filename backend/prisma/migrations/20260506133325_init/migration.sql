1c7e28d821f100869d68fa6a97587a48ebbf2eb8
Paul <pribolzi@k2r2p4.42mulhouse.fr>
2026-05-06 15:59:05 +0200

    ADD : New table to contain daily champion

---
+++ backend/prisma/migrations/20260506133325_init/migration.sql
@@ -0,0 +1,145 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT,
+    "avatar_url" TEXT,
+    "is_online" BOOLEAN NOT NULL DEFAULT false,
+    "is_two_factor_enabled" BOOLEAN NOT NULL DEFAULT false,
+    "two_factor_secret" TEXT,
+    "oauth_provider" TEXT,
+    "oauth_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Friendship" (
+    "id" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "requester_id" TEXT NOT NULL,
+    "addressee_id" TEXT NOT NULL,
+
+    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Match" (
+    "id" TEXT NOT NULL,
+    "game_mode" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'ongoing',
+    "played_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "target_champion_id" TEXT NOT NULL,
+
+    CONSTRAINT "Match_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Match_Participant" (
+    "id" TEXT NOT NULL,
+    "result" TEXT,
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "match_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Match_Participant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Guess" (
+    "id" TEXT NOT NULL,
+    "attempt_number" INTEGER NOT NULL,
+    "comparison_result" JSONB NOT NULL,
+    "match_id" TEXT NOT NULL,
+    "champion_id" TEXT NOT NULL,
+    "participant_id" TEXT NOT NULL,
+
+    CONSTRAINT "Guess_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Champion" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "position" TEXT NOT NULL,
+    "species" TEXT NOT NULL,
+    "resource_type" TEXT NOT NULL,
+    "range_type" TEXT NOT NULL,
+    "region" TEXT NOT NULL,
+    "release_year" INTEGER NOT NULL,
+
+    CONSTRAINT "Champion_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Daily_Reward" (
+    "id" TEXT NOT NULL,
+    "streak_count" INTEGER NOT NULL DEFAULT 0,
+    "points_earned" INTEGER NOT NULL DEFAULT 0,
+    "xp_points" INTEGER NOT NULL DEFAULT 0,
+    "level" INTEGER NOT NULL DEFAULT 0,
+    "elo_rating" INTEGER NOT NULL DEFAULT 1000,
+    "reward_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "Daily_Reward_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DailyMatch" (
+    "id" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "championId" TEXT NOT NULL,
+
+    CONSTRAINT "DailyMatch_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_oauth_id_key" ON "User"("oauth_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Champion_name_key" ON "Champion"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "DailyMatch_date_key" ON "DailyMatch"("date");
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_requester_id_fkey" FOREIGN KEY ("requester_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_addressee_id_fkey" FOREIGN KEY ("addressee_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Match" ADD CONSTRAINT "Match_target_champion_id_fkey" FOREIGN KEY ("target_champion_id") REFERENCES "Champion"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Match_Participant" ADD CONSTRAINT "Match_Participant_match_id_fkey" FOREIGN KEY ("match_id") REFERENCES "Match"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Match_Participant" ADD CONSTRAINT "Match_Participant_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Guess" ADD CONSTRAINT "Guess_match_id_fkey" FOREIGN KEY ("match_id") REFERENCES "Match"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Guess" ADD CONSTRAINT "Guess_champion_id_fkey" FOREIGN KEY ("champion_id") REFERENCES "Champion"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Guess" ADD CONSTRAINT "Guess_participant_id_fkey" FOREIGN KEY ("participant_id") REFERENCES "Match_Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Daily_Reward" ADD CONSTRAINT "Daily_Reward_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DailyMatch" ADD CONSTRAINT "DailyMatch_championId_fkey" FOREIGN KEY ("championId") REFERENCES "Champion"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
