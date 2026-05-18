c2fe1be4f2e75766a84b49d3044d344d1c35232d
Paul <pribolzi@k1r3p15.42mulhouse.fr>
2026-05-18 18:05:26 +0200

    ADD: Dailymatches module in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { HealthModule } from './health/health.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { ChampionsModule } from './champions/champions.module';
+import { DailymatchesModule } from './dailymatches/dailymatches.module';
 
 @Module({
-  imports: [HealthModule, UsersModule, PrismaModule, ChampionsModule],
+  imports: [HealthModule, UsersModule, PrismaModule, ChampionsModule, DailymatchesModule],
 })
 export class AppModule {}
