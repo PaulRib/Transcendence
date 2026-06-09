122d09b4776a7a92fa331ac2b142767c7fd40d3b
vitamine07 <amine.b67@hotmail.com>
2026-06-09 11:01:21 +0200

    resolve conflict

--- frontend/src/auth/ProtectedRoute.tsx
+++ frontend/src/auth/ProtectedRoute.tsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { Navigate } from 'react-router-dom';
-import { useAuth } from './AuthContext';
-import LoadingScreen from '../components/LoadingScreen';
-
-type ProtectedRouteProps = {
-    children: React.ReactNode;
-};
-
-export function ProtectedRoute({ children }: ProtectedRouteProps) {
-    const { currentUser, isLoading } = useAuth();
-
-    if (isLoading) {
-        return <LoadingScreen />;
-=======
 import { useAuth } from "./AuthContext";
 import { Outlet, Navigate } from "react-router-dom";
 
@@ -21,17 +6,11 @@ export function ProtectedRoute() {
 
     if (isLoading) {
         return null;
->>>>>>> main
     }
 
     if (!currentUser) {
         return <Navigate to="/login" replace />;
     }
 
-<<<<<<< HEAD
-    return <>{children}</>;
-}
-=======
     return <Outlet />;
 }
->>>>>>> main
