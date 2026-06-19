919a0ade7f862ef4cb19a5d732c8be1d4e509bd9
Mehdi El Warrad <meel-war@k2r3p10.42mulhouse.fr>
2026-06-19 19:12:32 +0200

    Add chat module structure

---
+++ backend/src/chat/chat.module.ts
@@ -0,0 +1,19 @@
+import { Module } from "@nestjs/common";
+import { ChatController } from "./chat.controller";
+import { ChatService } from "./chat.service";
+import { PrismaModule } from "../../prisma/prisma.module";
+import { JwtModule } from "@nestjs/jwt";
+
+
+@Module({
+    imports: [PrismaModule, JwtModule.register({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+            expiresIn: '1h',
+            },
+        }),
+    ],
+    controllers: [ChatController],
+    providers: [ChatService],
+})
+export class ChatModule {}
