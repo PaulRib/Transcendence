121175ffb71bde8d7cef21b528866924809f110c
Paul <paul.ribolzi@gmail.com>
2026-07-06 18:42:08 +0200

    FIX: daily reward feature is now calcultated in backend during guess to avoid cheat

--- backend/src/dailymatches/dailymatches.module.ts
+++ backend/src/dailymatches/dailymatches.module.ts
@@ -3,9 +3,13 @@ import { PrismaModule } from '../../prisma/prisma.module';
 import { ChampionsModule } from '../champions/champions.module';
 import { DailymatchesController } from './dailymatches.controller';
 import { DailymatchesService } from './dailymatches.service';
+import { GamificationModule } from '../gamification/gamification.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule, ChampionsModule],
+  imports: [PrismaModule, ChampionsModule, GamificationModule, 	JwtModule.register({
+			  secret: process.env.JWT_SECRET,
+		  }),],
   controllers: [DailymatchesController],
   providers: [DailymatchesService],
   exports: [DailymatchesService],
