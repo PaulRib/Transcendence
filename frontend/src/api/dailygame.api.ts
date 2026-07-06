063c5186bbddeeea0ff3912f6c55888fad8c3c30
Paul <paul.ribolzi@gmail.com>
2026-07-06 18:43:53 +0200

    FIX: daily reward feature is now calcultated in backend during guess to avoid cheat

--- frontend/src/api/dailygame.api.ts
+++ frontend/src/api/dailygame.api.ts
@@ -1,26 +1,14 @@
 import { API_BASE_URL } from "../config/api";
-import type { ChampionDay, GuessResponse, DailyData } from "./type.api";
+import type { GuessResponse, DailyData } from "./type.api";
 
-
-export async function getDailyChamp(): Promise<ChampionDay> {
-    const response = await fetch(`${API_BASE_URL}/dailyMatches/champ`);
-
-    if (!response.ok) {
-        throw new Error('Champion names request failed');
-    }
-
-    const data: ChampionDay = await response.json();
-
-    return data;
-}
-
-export async function sendGuess(championName: string): Promise<GuessResponse> {
+export async function sendGuess(attempts: number, championName: string): Promise<GuessResponse> {
 	const response = await fetch(`${API_BASE_URL}/dailyMatches/guess`, {
 		method: 'POST',
 		headers: { 
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify({ name: championName}),
+		credentials: 'include',
+		body: JSON.stringify({ attempts: attempts, name: championName}),
 	});
 	if (!response.ok) {
 		throw new Error('Failed to submit guess');
